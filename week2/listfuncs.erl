ea31cacbb019bf2756b3cc1db5b47def5e85410a
Miel Donkers <miel.donkers@codecentric.nl>
2017-03-05 11:07:28 +0100

    Added second week assignments

---
+++ week2/listfuncs.erl
@@ -0,0 +1,41 @@
+%%%-------------------------------------------------------------------
+%%% @author Miel Donkers
+%%% @copyright (C) 2017, codecentric
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2017-03-05 10:11:43.464037
+%%%-------------------------------------------------------------------
+-module(listfuncs).
+
+%% Application callbacks
+-export([product/1,listmax/1,double/1,evens/1]).
+
+product(Xs) ->
+  product(Xs, 1).
+
+product([], S) ->
+  S;
+product([X|Xs], S) ->
+  product(Xs, S*X).
+
+listmax(Xs) ->
+  listmax(Xs, 0).
+
+listmax([], Max) ->
+  Max;
+listmax([X|Xs], Max) ->
+  listmax(Xs, max(Max, X)).
+
+double([]) ->
+  [];
+double([X|Xs]) ->
+  [X*2|double(Xs)].
+
+evens([]) ->
+  [];
+evens([X|Xs]) when X rem 2 == 0 ->
+    [X|evens(Xs)];
+evens([_|Xs]) ->
+    evens(Xs).
+
