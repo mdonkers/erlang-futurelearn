cb1a230072c03db6a9911b572ca6a024453b833d
Miel Donkers <miel.donkers@codecentric.nl>
2017-03-09 16:12:32 +0100

    Finished assignment to filter words with lines

--- week2/index.erl
+++ week2/index.erl
@@ -13,7 +13,65 @@
 get_file_contents(Name) ->
   {ok,File} = file:open(Name,[read]),
   Rev = get_all_lines(File,[]),
-  lists:reverse(Rev).
+  FilteredLines = remove_punct_caps(lists:reverse(Rev)),
+  combine(lists:flatten(get_words(FilteredLines,1))).
+
+combine(Words) ->
+  Fun      = fun(Key) -> {Key,proplists:get_all_values(Key,Words)} end,
+  lists:map(Fun,proplists:get_keys(Words)).
+
+
+get_words([], _LineNr) ->
+  [];
+get_words([X|Xs],LineNr) ->
+  [convert_words_from_line(filter_common(string:tokens(X, " ")), LineNr) | get_words(Xs, LineNr + 1)].
+
+convert_words_from_line([], _LineNr) ->
+  [];
+convert_words_from_line([X|Xs], LineNr) ->
+  [{X, LineNr}|convert_words_from_line(Xs, LineNr)].
+
+filter_common([]) ->
+  [];
+filter_common([X|Xs]) ->
+  case string:len(X) > 2 of
+    true ->
+      [X|filter_common(Xs)];
+    false ->
+      filter_common(Xs)
+  end.
+
+remove_punct_caps([]) ->
+  [];
+remove_punct_caps([X|Xs]) ->
+  [nocaps(nopunct(X)) | remove_punct_caps(Xs)].
+
+% Remove punctuation
+nopunct([]) ->
+    [];
+nopunct([X|Xs]) ->
+    case lists:member(X,".,;:-\t\n\'\"\\") of
+	true ->
+	    nopunct(Xs);
+	false ->
+	    [ X | nopunct(Xs) ]
+    end.
+
+
+% change all to smallcaps
+nocaps([]) ->
+    [];
+nocaps([X|Xs]) ->
+    [ nocap(X) | nocaps(Xs) ].
+
+nocap(X) ->
+    case $A =< X andalso X =< $Z of
+	true ->
+	    X+32;
+	false ->
+	    X
+    end.
+
 
 % Auxiliary function for get_file_contents.
 % Not exported.
